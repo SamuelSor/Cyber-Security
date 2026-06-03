f097300d0838829d4daeed643c429800ad72ab74
Sam S <167921656+SamuelSor@users.noreply.github.com>
2026-06-03 16:30:50 -0400

    Create WN11-SO-000220.ps1

---
+++ STIGS/WN11-SO-000220.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+    This PowerShell script disables any printing with HTTP connections
+
+.NOTES
+    Author          : Samuel Sorto
+    LinkedIn        : linkedin.com/in/SamuelSorto
+    GitHub          : github.com/samuelsor
+    Date Created    : 2026-06-03
+    Last Modified   : 2026-06-03
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-SO-000220
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-SO-000220/
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+#>
+
+# Define the registry path and values
+# Create the registry path if it does not already exist
+$RegistryPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+
+if (-not (Test-Path $RegistryPath)) {
+    New-Item -Path $RegistryPath -Force | Out-Null
+}
+
+# Set fPromptForPassword to Enabled (1) STILL NEEDS TESTING
+New-ItemProperty `
+    -Path $RegistryPath `
+    -Name "fPromptForPassword" `
+    -PropertyType DWord `
+    -Value 1 `
+    -Force | Out-Null
+
+Write-Host "fPromptForPassword has been set to 1." -ForegroundColor Green
