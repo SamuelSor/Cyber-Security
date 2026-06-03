c9056e3cc9058188218cb7dc37d323d468ff3722
Sam S <167921656+SamuelSor@users.noreply.github.com>
2026-06-03 16:19:01 -0400

    Create WN11-CC-000100.ps1

---
+++ STIGS/WN11-CC-000100.ps1
@@ -0,0 +1,43 @@
+<#
+.SYNOPSIS
+    This PowerShell script disables any printing with HTTP connections
+
+.NOTES
+    Author          : Samuel Sorto
+    LinkedIn        : linkedin.com/in/SamuelSorto
+    GitHub          : github.com/samuelsor
+    Date Created    : 2026-06-03
+    Last Modified   : 2026-06-03
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN11-CC-000100
+    Documentation   : https://stigaview.com/products/win11/v2r7/WN11-CC-000100/
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+#>
+
+# Define the registry path and values
+# Create the registry path if it does not already exist
+$RegistryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+
+if (-not (Test-Path $RegistryPath)) {
+    New-Item -Path $RegistryPath -Force | Out-Null
+}
+
+# Set NoAutorun to Enabled (1)
+New-ItemProperty `
+    -Path $RegistryPath `
+    -Name "NoAutorun" `
+    -PropertyType DWord `
+    -Value 1 `
+    -Force | Out-Null
+
+Write-Host "NoAutorun has been set to 1." -ForegroundColor Green
